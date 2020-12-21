1c79947f69fb099bef5350cbc3f098c0789bcb9f
Ivan Hofer <ivan.hofer@outlook.com>
2020-12-21 18:26:01 +0100

    add better types for boolean filters
    #3

--- src/boolean/boolean.ts
+++ src/boolean/boolean.ts
@@ -1,29 +1,44 @@
-import type { Falsy, Truthy } from '../types'
+import type {
+	Empty,
+	False,
+	Falsy,
+	NotEmpty,
+	NotFalse,
+	NotNull,
+	NotTrue,
+	NotUndefined,
+	NotZero,
+	Null,
+	True,
+	Truthy,
+	Undefined,
+	Zero,
+} from '../types'
 
 export const isTruthy = <T>(value: T): value is Truthy<T> => !!value
 
 export const isFalsy = <T>(value: T): value is Falsy<T> => !value
 
-export const isUndefined = <T>(value: T): value is Falsy<T> => value === undefined
+export const isUndefined = <T>(value: T): value is Undefined<T> => value === undefined
 
-export const isNotUndefined = <T>(value: T): value is Truthy<T> => value !== undefined
+export const isNotUndefined = <T>(value: T): value is NotUndefined<T> => value !== undefined
 
-export const isNull = <T>(value: T): value is Falsy<T> => value === null
+export const isNull = <T>(value: T): value is Null<T> => value === null
 
-export const isNotNull = <T>(value: T): value is Truthy<T> => value !== null
+export const isNotNull = <T>(value: T): value is NotNull<T> => value !== null
 
-export const isTrue = <T>(value: T): value is Truthy<T> => <unknown>value === true
+export const isTrue = <T>(value: T): value is True<T> => <unknown>value === true
 
-export const isNotTrue = <T>(value: T): value is Falsy<T> => <unknown>value !== true
+export const isNotTrue = <T>(value: T): value is NotTrue<T> => <unknown>value !== true
 
-export const isFalse = <T>(value: T): value is Falsy<T> => <unknown>value === false
+export const isFalse = <T>(value: T): value is False<T> => <unknown>value === false
 
-export const isNotFalse = <T>(value: T): value is Truthy<T> => <unknown>value !== false
+export const isNotFalse = <T>(value: T): value is NotFalse<T> => <unknown>value !== false
 
-export const isZero = <T>(value: T): value is Falsy<T> => <unknown>value === 0
+export const isZero = <T>(value: T): value is Zero<T> => <unknown>value === 0
 
-export const isNotZero = <T>(value: T): value is Truthy<T> => <unknown>value !== 0
+export const isNotZero = <T>(value: T): value is NotZero<T> => <unknown>value !== 0
 
-export const isEmpty = <T>(value: T): value is Falsy<T> => <unknown>value === ''
+export const isEmpty = <T>(value: T): value is Empty<T> => <unknown>value === ''
 
-export const isNotEmpty = <T>(value: T): value is Truthy<T> => <unknown>value !== ''
+export const isNotEmpty = <T>(value: T): value is NotEmpty<T> => <unknown>value !== ''
