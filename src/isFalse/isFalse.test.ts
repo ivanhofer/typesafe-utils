598a0a2e81ff355262f9b98f4da0df1d2f65daf1
Ivan Hofer <ivan.hofer@outlook.com>
2020-12-23 16:00:02 +0100

    add isPropertyFalse function
    #15

--- src/isFalse/isFalse.test.ts
+++ src/isFalse/isFalse.test.ts
@@ -2,14 +2,14 @@ import { suite } from 'uvu'
 import * as assert from 'uvu/assert'
 import { Everything } from '../types'
 
-import { isFalse, isNotFalse } from './isFalse'
+import { isFalse, isNotFalse, isPropertyFalse, isPropertyNotFalse } from './isFalse'
 
 const test = suite('false')
 
 const falseValues = [false]
 const notFalseValues = [-Infinity, -1, 1, Infinity, 'test', true, [], {}, undefined, null, 0, '']
 
-// isFalse -----------------------------------------------------------------------------------------------------
+// isFalse ------------------------------------------------------------------------------------------------------------
 
 falseValues.forEach((value) => test(`isFalse ${value}`, () => assert.ok(isFalse(value))))
 notFalseValues.forEach((value) => test(`! isFalse ${value}`, () => assert.not(isFalse(value))))
@@ -17,12 +17,30 @@ notFalseValues.forEach((value) => test(`! isFalse ${value}`, () => assert.not(is
 const _falseType: false[] = [...falseValues, notFalseValues].filter(isFalse)
 const _neverType: never[] = notFalseValues.filter(isFalse)
 
-// isNotFalse -----------------------------------------------------------------------------------------------------
+// isNotFalse ---------------------------------------------------------------------------------------------------------
 
 notFalseValues.forEach((value) => test(`isNotFalse ${value}`, () => assert.ok(isNotFalse(value))))
 falseValues.forEach((value) => test(`! isNotFalse ${value}`, () => assert.not(isNotFalse(value))))
 
 const _allNotType: Exclude<Everything, false>[] = [...falseValues, ...notFalseValues].filter(isNotFalse)
 // const _neverNotType: never[] = falseValues.filter(isNotFalse)
+
+// isPropertyFalse ----------------------------------------------------------------------------------------------------
+
+test(`isPropertyTrue available`, () => {
+	const items = [{ available: false }, { available: '' }, { available: undefined }]
+	const filteredItems = items.filter(isPropertyFalse('available'))
+
+	assert.ok(filteredItems.length === 1)
+})
+
+// isPropertyNotFalse -------------------------------------------------------------------------------------------------
+
+test(`isPropertyNotFalse available`, () => {
+	const items = [{ available: false }, { available: true }, { available: null }]
+	const filteredItems = items.filter(isPropertyNotFalse('available'))
+
+	assert.ok(filteredItems.length === 2)
+})
 
 test.run()
