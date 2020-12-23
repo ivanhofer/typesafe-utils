d4c8b71b81ed80da0288ccb00967d803162a2626
Ivan Hofer <ivan.hofer@outlook.com>
2020-12-23 16:02:51 +0100

    add isPropertyEmpty function
    #15

--- src/isEmpty/isEmpty.test.ts
+++ src/isEmpty/isEmpty.test.ts
@@ -2,14 +2,14 @@ import { suite } from 'uvu'
 import * as assert from 'uvu/assert'
 import { Everything } from '../types'
 
-import { isEmpty, isNotEmpty } from './isEmpty'
+import { isEmpty, isNotEmpty, isPropertyEmpty, isPropertyNotEmpty } from './isEmpty'
 
 const test = suite('empty')
 
 const emptyValues = ['']
 const notEmptyValues = [-Infinity, -1, 1, Infinity, 'test', true, [], {}, undefined, null, 0, false]
 
-// isEmpty -----------------------------------------------------------------------------------------------------
+// isEmpty ------------------------------------------------------------------------------------------------------------
 
 emptyValues.forEach((value) => test(`isEmpty ${value}`, () => assert.ok(isEmpty(value))))
 notEmptyValues.forEach((value) => test(`! isEmpty ${value}`, () => assert.not(isEmpty(value))))
@@ -17,12 +17,30 @@ notEmptyValues.forEach((value) => test(`! isEmpty ${value}`, () => assert.not(is
 const _emptyType: ''[] = [...emptyValues, notEmptyValues].filter(isEmpty)
 const _neverType: never[] = notEmptyValues.filter(isEmpty)
 
-// isNotEmpty -----------------------------------------------------------------------------------------------------
+// isNotEmpty ---------------------------------------------------------------------------------------------------------
 
 notEmptyValues.forEach((value) => test(`isNotEmpty ${value}`, () => assert.ok(isNotEmpty(value))))
 emptyValues.forEach((value) => test(`! isNotEmpty ${value}`, () => assert.not(isNotEmpty(value))))
 
 const _allNotType: Exclude<Everything, ''>[] = [...emptyValues, ...notEmptyValues].filter(isNotEmpty)
 const _neverNotType: never[] = emptyValues.filter(isNotEmpty)
+
+// isPropertyEmpty ----------------------------------------------------------------------------------------------------
+
+test(`isPropertyEmpty name`, () => {
+	const items = [{ name: 'name-1' }, { name: '' }, { name: undefined }]
+	const filteredItems = items.filter(isPropertyEmpty('name'))
+
+	assert.ok(filteredItems.length === 1)
+})
+
+// isPropertyNotEmpty -------------------------------------------------------------------------------------------------
+
+test(`isPropertyNotEmpty name`, () => {
+	const items = [{ name: null }, { name: 'name-2' }, { name: '' }]
+	const filteredItems = items.filter(isPropertyNotEmpty('name'))
+
+	assert.ok(filteredItems.length === 2)
+})
 
 test.run()
--- src/isEmpty/isEmpty.ts
+++ src/isEmpty/isEmpty.ts
@@ -3,3 +3,9 @@ import type { Empty, NotEmpty } from '../types'
 export const isEmpty = <T>(value: T | ''): value is Empty<T> => <unknown>value === ''
 
 export const isNotEmpty = <T>(value: T): value is NotEmpty<T> => <unknown>value !== ''
+
+export const isPropertyEmpty = <T, K extends keyof T>(property: K) => (value: T): boolean =>
+	<unknown>value[property] === ''
+
+export const isPropertyNotEmpty = <T, K extends keyof T>(property: K) => (value: T): boolean =>
+	<unknown>value[property] !== ''
