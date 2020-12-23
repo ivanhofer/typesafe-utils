1381d937a3549de4f2014459e9690a4ef8dcb373
Ivan Hofer <ivan.hofer@outlook.com>
2020-12-23 15:29:00 +0100

    fix typo
    #4

--- src/filterDuplicates/filterDuplicates.test.ts
+++ src/filterDuplicates/filterDuplicates.test.ts
@@ -1,6 +1,6 @@
 import { suite } from 'uvu'
 import * as assert from 'uvu/assert'
-import { filterDuplcates, filterDuplcatesByKey } from './filterDuplicates'
+import { filterDuplicates, filterDuplicatesByKey } from './filterDuplicates'
 
 const test = suite('filterDuplicates')
 
@@ -8,7 +8,7 @@ const test = suite('filterDuplicates')
 
 test(`filter duplicate numbers`, () => {
 	const items = [1, 2, 3, 5, 8, 1]
-	const filteredItems = items.filter(filterDuplcates)
+	const filteredItems = items.filter(filterDuplicates)
 
 	assert.ok(filteredItems.length === 5)
 	assert.ok(filteredItems.filter((nr) => nr === 1).length === 1)
@@ -18,7 +18,7 @@ test(`filter duplicate objects`, () => {
 	const item1 = { id: 1 }
 	const item2 = { id: 2 }
 	const items = [item1, item2, item1]
-	const filteredItems = items.filter(filterDuplcates)
+	const filteredItems = items.filter(filterDuplicates)
 
 	assert.ok(filteredItems.length === 2)
 	assert.ok(filteredItems.includes(item2))
@@ -29,7 +29,7 @@ test(`filter duplicate objects`, () => {
 
 test(`filter duplicate IDs`, () => {
 	const items = [{ id: 1 }, { id: 2 }, { id: 1 }]
-	const filteredItems = items.filter(filterDuplcatesByKey('id'))
+	const filteredItems = items.filter(filterDuplicatesByKey('id'))
 
 	assert.ok(filteredItems.length === 2)
 	assert.ok(filteredItems.filter(({ id }) => id === 1).length === 1)
@@ -42,7 +42,7 @@ test(`filter duplicate names`, () => {
 		{ id: 3, name: 'name-1' },
 		{ id: 4, name: 'name-2' },
 	]
-	const filteredItems = items.filter(filterDuplcatesByKey('name'))
+	const filteredItems = items.filter(filterDuplicatesByKey('name'))
 
 	assert.ok(filteredItems.length === 2)
 	assert.ok(filteredItems.filter(({ name }) => name === 'name-1').length === 1)
--- src/filterDuplicates/filterDuplicates.ts
+++ src/filterDuplicates/filterDuplicates.ts
@@ -1,4 +1,4 @@
-export const filterDuplcates = <T>(value: T, i: number, a: T[]): boolean => a.findIndex((t) => t === value) === i
+export const filterDuplicates = <T>(value: T, i: number, a: T[]): boolean => a.findIndex((t) => t === value) === i
 
-export const filterDuplcatesByKey = <T, K extends keyof T>(key: K) => (value: T, i: number, a: T[]): boolean =>
+export const filterDuplicatesByKey = <T, K extends keyof T>(key: K) => (value: T, i: number, a: T[]): boolean =>
 	a.findIndex((t) => t[key] === value[key]) === i
