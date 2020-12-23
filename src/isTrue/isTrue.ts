9d8a550b078696a55a550b6c889ca1cf60ac709b
Ivan Hofer <ivan.hofer@outlook.com>
2020-12-23 15:55:59 +0100

    add isPropertyTrue function
    #15

--- src/isTrue/isTrue.ts
+++ src/isTrue/isTrue.ts
@@ -3,3 +3,9 @@ import { True, NotTrue } from '../types'
 export const isTrue = <T>(value: T): value is True<T> => <unknown>value === true
 
 export const isNotTrue = <T>(value: T): value is NotTrue<T> => <unknown>value !== true
+
+export const isPropertyTrue = <T, K extends keyof T>(property: K) => (value: T): boolean =>
+	<unknown>value[property] === true
+
+export const isPropertyNotTrue = <T, K extends keyof T>(property: K) => (value: T): boolean =>
+	<unknown>value[property] !== true
