b372cce4436a65c66f323d266cfd4d286ec72df5
Ivan Hofer <ivan.hofer@outlook.com>
2021-01-06 15:20:05 +0100

    mark index and array parameter optional

--- src/logical/logical.ts
+++ src/logical/logical.ts
@@ -1,21 +1,21 @@
 type FilterFn<T, U extends T = T> =
-	| ((value: T, i: number, all: T[]) => boolean)
-	| ((value: T, i: number, all: T[]) => value is U)
+	| ((value: T, i?: number, array?: T[]) => boolean)
+	| ((value: T, i?: number, array?: T[]) => value is U)
 
 export const and = <T, U extends T = T>(...filters: FilterFn<any, any>[]) => (
 	value: T,
-	index: number,
-	all: T[],
-): value is U => filters.reduce((prev, filter) => prev && (filter as any)(value, index, all), true)
+	index?: number,
+	array?: T[],
+): value is U => filters.reduce((prev, filter) => prev && (filter as any)(value, index, array), true)
 
 export const or = <T, U extends T = T>(...filters: FilterFn<any, any>[]) => (
 	value: T,
-	index: number,
-	all: T[],
-): value is U => filters.reduce((prev, filter) => prev || (filter as any)(value, index, all), false)
+	index?: number,
+	array?: T[],
+): value is U => filters.reduce((prev, filter) => prev || (filter as any)(value, index, array), false)
 
 export const not = <T, U extends T = T, F extends FilterFn<T, U> = FilterFn<T, U>>(filterFn: F) => (
 	value: T,
-	i: number,
-	all: T[],
-): value is U => !filterFn(value, i, all)
+	i?: number,
+	array?: T[],
+): value is U => !filterFn(value, i, array)
